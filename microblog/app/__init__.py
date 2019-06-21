75cd74c09cf650a0fd19ecd095e05cd0befdf987
Lee Wiegman <lwiegma1@progressive.com>
2019-06-21 16:02:10 -0400

    Working on user logins chapter 5

--- microblog/app/__init__.py
+++ microblog/app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -9,3 +10,7 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
 from app import routes, models
+
+login = LoginManager(app)
+login.login_view = 'login'
+
